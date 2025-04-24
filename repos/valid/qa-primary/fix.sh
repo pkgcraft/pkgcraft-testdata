00b1e5a9a1fc07b40f9d3cd161d927bcceeba36e
Tim Harder <radhermit@gmail.com>
2025-04-24 16:32:36 -0600

    repos: use script instead of patch for file removal

---
+++ repos/valid/qa-primary/fix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+rm CategoryEmpty/PackageEmpty/.empty
+rm eclass/unused.eclass
+rm licenses/deprecated
+rm licenses/unused
+rm profiles/package.deprecated
+rm -r stub/deprecated
