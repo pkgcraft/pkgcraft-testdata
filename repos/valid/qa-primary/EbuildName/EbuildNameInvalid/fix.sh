fbc76424ac072d66cc4c18736d4044da520b601f
Tim Harder <radhermit@gmail.com>
2025-04-24 16:17:07 -0600

    repos: replace patch with script for EbuildNameInvalid tests

---
+++ repos/valid/qa-primary/EbuildName/EbuildNameInvalid/fix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+mv EbuildNameInvalid-0-1.ebuild EbuildNameInvalid-0.1.ebuild
+mv EbuildNameInvalid1-2.ebuild EbuildNameInvalid-1.2.ebuild
+mv EbuildNameInvalid-1.a.ebuild EbuildNameInvalid-1a.ebuild
+rm EbuildNameInvalid.ebuild
